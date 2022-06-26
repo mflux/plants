3d6df5f88d6ae7a28edcb5c7547ae1b4fbffa463
mflux <flux.blackcat@gmail.com>
2022-06-26 21:19:38 +0100

    generate better looking terrain

--- sketch.js
+++ sketch.js
@@ -36,16 +36,29 @@ function setup() {
   canvas.style("image-rendering", "pixelated");
 
   // Generate some soil.
-  const earthNoiseScale = 0.01;
-  grids.earth.replaceEachXYValue((x, y, _) => {
-    const v = noise(x * earthNoiseScale * 10, y * earthNoiseScale * 20);
-    if (y < v * SIM_HEIGHT) {
+  const earthNoiseScale1 = 0.001;
+  const earthNoiseScale2 = 0.05;
+  grids.earth.fill(SoilType.None);
+  for (let x = 0; x < grids.earth.width; x++){
+    const v = int(noise(x * earthNoiseScale1) * grids.earth.height * 0.5);
+    const v2 = int(noise(x * earthNoiseScale2) * 20);
+    for (let y = grids.earth.height * 0.3 + v + v2; y < grids.earth.height; y++){
+      grids.earth.set(x, y, SoilType.Soft);
+    }
+  }
+
+  const rockNoiseScale = 0.02;
+  grids.earth.replaceEachXYValue((x, y, value) => {
+    if (value === SoilType.Soft) {
+      const v = noise(x * rockNoiseScale * 10, y * rockNoiseScale * 20);
+      if (v > 0.6) {
+        return SoilType.Hard;
+      }
+      return SoilType.Soft;
+    }
+    else {
       return SoilType.None;
     }
-    if (v > 0.6) {
-      return SoilType.Hard;
-    }
-    return SoilType.Soft;
   });
 
   // Generate moisture.
